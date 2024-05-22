aa411f0afdd9715db9316f11b94abcc9675179bd
Ritesh Mahajan <rdmahajan1297@gmail.com>
2024-05-22 19:03:20 +0530

    login form

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeUser = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setLogin({
+      ...login,
+      [name]: value,
+    });
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log({ login });
+  };
+
+  return (
+    <div>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/login.png"
+                  alt="login page"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Login Form</h1>
+                <br />
+                <form onSubmit={onSubmitForm}>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email"
+                      required
+                      id="email"
+                      autoComplete="off"
+                      value={login.email}
+                      onChange={onChangeUser}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      required
+                      id="password"
+                      autoComplete="off"
+                      value={login.password}
+                      onChange={onChangeUser}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const onChangeUser = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log({ user });
+  };
+
+  return (
+    <div>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt="registration page"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Registration Form</h1>
+                <br />
+                <form onSubmit={onSubmitForm}>
+                  <div>
+                    <label htmlFor="username">Username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="Username"
+                      autoComplete="off"
+                      required
+                      id="username"
+                      value={user.username}
+                      onChange={onChangeUser}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter a email"
+                      autoComplete="off"
+                      required
+                      id="email"
+                      value={user.email}
+                      onChange={onChangeUser}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      placeholder="Enter a phone"
+                      autoComplete="off"
+                      required
+                      id="phone"
+                      value={user.phone}
+                      onChange={onChangeUser}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Enter a password"
+                      autoComplete="off"
+                      required
+                      id="password"
+                      value={user.password}
+                      onChange={onChangeUser}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </div>
+  );
+};
+
+export default Register;
