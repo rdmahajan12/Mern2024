8c4c451762a1873c689b1819ba923988b2ef89d6
Ritesh Mahajan <rdmahajan1297@gmail.com>
2024-05-24 19:17:45 +0530

    connect with database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const authRouter = require("./router/auth-router");
 const contactRouter = require("./router/contact-router");
 const connectDB = require("./utils/db");
 const errorMiddleware = require("./middleware/error-middleware");
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE, PATCH",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/form", contactRouter);
