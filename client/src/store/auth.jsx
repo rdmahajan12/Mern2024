480defe487276808148912abcd6b34cfa5ff40f3
Ritesh Mahajan <rdmahajan1297@gmail.com>
2024-05-31 18:41:14 +0530

    remove hosting

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [service, setService] = useState([]);
   const authToken = `Bearer ${token}`;
-  const API = "https://mern2024-nine.vercel.app";
+  const API = "http://localhost:3000";
 
   const setServerToken = (serverToken) => {
     setToken(serverToken);
